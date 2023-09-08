493ce2112d4aecf8a93aafba9d25bda0436b022b
acaylor <ajcaylor@gmail.com>
2023-09-08 12:58:40 -0600

    debian terraform install

---
+++ debian-terraform.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "importing hashicorp gpg key"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "add hashicorp repo"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
