ec7290a03915c78d2e98b090f9362523a4edaef0
acaylor <ajcaylor@gmail.com>
2025-09-26 11:28:48 -0600

    update motd script without fetch scripts

--- motd.sh
+++ motd.sh
@@ -1,43 +1,91 @@
 #!/bin/bash
 USER=$(whoami)
 HOSTNAME=$(uname -n)
-uptime=$(cat /proc/uptime | cut -f1 -d.)
-upDays=$(($uptime/60/60/24))
-upHours=$(($uptime/60/60%24))
-upMins=$(($uptime/60%60))
-upSecs=$(($uptime%60))
-LOAD1=$(cat /proc/loadavg | awk {'print $1'})
-LOAD5=$(cat /proc/loadavg | awk {'print $2'})
-LOAD15=$(cat /proc/loadavg | awk {'print $3'})
 USERS=$(users | wc -w)
-#RELEASE=$(cat /etc/redhat-release || uname -sr)
-WEATHER=$(curl -s wttr.in/Denver?format=3)
+
+# System uptime
+read uptime_raw idle_raw < /proc/uptime
+uptime=${uptime_raw%.*}  # Remove decimal part, keep only uptime (first value)
+upDays=$((uptime/60/60/24))
+upHours=$((uptime/60/60%24))
+upMins=$((uptime/60%60))
+upSecs=$((uptime%60))
+
+# Weather with timeout
+WEATHER=$(timeout 2 curl -s wttr.in/Denver?format=3 2>/dev/null || echo "Weather unavailable")
+
+# Time greeting
 HOUR=$(date +"%H")
-if [ $HOUR -lt 12 -a $HOUR -ge 0 ]
-then TIME="morning"
-elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]
-then TIME="afternoon"
+case $HOUR in
+  0[0-9]|1[01]) TIME="morning" ;;
+  1[2-6]) TIME="afternoon" ;;
+  *) TIME="evening" ;;
+esac
+
+# System metrics
+read load1 load5 load15 _ < /proc/loadavg
+
+# Memory calculation (handle missing MemAvailable on older systems)
+if grep -q "MemAvailable" /proc/meminfo; then
+    read -r _ mem_total _ < /proc/meminfo
+    read -r _ mem_available _ < <(grep MemAvailable /proc/meminfo)
+    mem_used_pct=$(( (mem_total - mem_available) * 100 / mem_total ))
 else
-        TIME="evening"
+    # Fallback for older systems without MemAvailable
+    mem_info=$(free | grep '^Mem:')
+    mem_total=$(echo $mem_info | awk '{print $2}')
+    mem_used=$(echo $mem_info | awk '{print $3}')
+    mem_used_pct=$(( mem_used * 100 / mem_total ))
 fi
 
+# Disk usage - escape parentheses properly
+DISK_USAGE=$(df -h / | awk 'NR==2 {print $3 "/" $2 " (" $5 ")"}')
+
+# Colors
 RED='\e[0;41m'
 NC='\e[0m'
 
-# Output below
+# Output
 printf "Good $TIME $USER\n"
 printf "
 ====================================================================================================
 \n"
-#/usr/bin/neofetch
-./pfetch
+
+# Check if macchina exists, otherwise show OS info
+if command -v macchina &>/dev/null; then
+    macchina
+else
+    # Get OS information
+    if [[ -f /etc/os-release ]]; then
+        source /etc/os-release
+        OS_NAME="$NAME"
+        OS_VERSION="$VERSION"
+    elif [[ -f /etc/lsb-release ]]; then
+        source /etc/lsb-release
+        OS_NAME="$DISTRIB_ID"
+        OS_VERSION="$DISTRIB_RELEASE"
+    else
+        OS_NAME=$(uname -s)
+        OS_VERSION="Unknown"
+    fi
+
+    KERNEL=$(uname -r)
+    ARCH=$(uname -m)
+
+    printf "   OS...........................................: $OS_NAME $OS_VERSION\n"
+    printf "   Kernel.......................................: $KERNEL\n"
+    printf "   Architecture.................................: $ARCH\n"
+    printf "   Hostname.....................................: $HOSTNAME\n"
+fi
+
 printf "
 ====================================================================================================
 \n"
-#printf "    Hostname........................................: $HOSTNAME\n"
-#printf "    Current user....................................: $USER\n"
+
 printf "   Users...........................................: Currently $USERS user(s) logged on\n"
-printf "   Load Average....................................: $LOAD1, $LOAD5, $LOAD15 (1, 5, 15 mins)\n"
+printf "   Load Average....................................: $load1, $load5, $load15 (1, 5, 15 mins)\n"
+printf "   Memory Usage....................................: ${mem_used_pct}%% used\n"
+printf "   Disk Usage......................................: %s\n" "$DISK_USAGE"
 printf "   Weather.........................................: $WEATHER\n"
 printf "
 ====================================================================================================
